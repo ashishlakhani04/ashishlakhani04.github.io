0f4c8d50ab663ba5a314cda02af814fe2e94d081
Ashish Lakhani <depashlakhani@gmail.com>
2018-09-21 21:43:25 +0530

    update inceptum

--- inceptum/js/main.js
+++ inceptum/js/main.js
@@ -4,30 +4,13 @@ function music() {
 	var s = document.getElementById("gggaudio");
 	if(countt%2===0)
 	{
-		document.getElementById("budio").src = '../inceptum/img/audioon.png';
+		document.getElementById("budio").src = 'file:///Users/jasbir/Downloads/ashishlakhani04.github.io-master%202/inceptum/img/audioon.png';
 		s.play();
 	}
 	else
 	{
-		document.getElementById("budio").src = '../inceptum/img/audioff.png';
+		document.getElementById("budio").src = 'file:///Users/jasbir/Downloads/ashishlakhani04.github.io-master%202/inceptum/img/audioff.png';
 		s.pause();
 	}
 	countt+=1;
-}
-
-var countt2=0;
-function music() {
-	var po = document.getElementById("budio").src;
-	var s = document.getElementById("gggaudio");
-	if(countt2%2===0)
-	{
-		document.getElementById("budio").src = '../../inceptum/img/audioon.png';
-		s.play();
-	}
-	else
-	{
-		document.getElementById("budio").src = '../../inceptum/img/audioff.png';
-		s.pause();
-	}
-	countt2+=1;
 }
